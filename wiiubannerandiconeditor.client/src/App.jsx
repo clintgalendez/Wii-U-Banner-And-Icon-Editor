23c644860661c9dba7f9361a1e991a7d1321eaa9
Clint-Galendez-20210021497 <20210021497@my.xu.edu.ph>
2025-05-26 10:17:03 +0800

    Replace fabric with react-konva for rendering
    Updated dependencies in `package-lock.json` and `package.json` to remove `fabric` and add `react-konva` and `use-image`. Modified `App.jsx` to utilize `react-konva` components for rendering, including a new image loading mechanism with the `useImage` hook. Cleaned up unused dependencies and added a new PNG image file `wii_u_icon_template.png`.

--- wiiubannerandiconeditor.client/src/App.jsx
+++ wiiubannerandiconeditor.client/src/App.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { StaticCanvas, FabricText } from 'fabric'
+import { Stage, Layer, Image } from 'react-konva';
+import useImage from 'use-image';
 
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -7,8 +8,9 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
-
 function App() {
+    const [iconOVerlay] = useImage('src/assets/templates/wii_u_icon_template.png');
+
     return (
         <Stack spacing={5} sx={{ alignItems: 'center', width: '100%'}}>
             <Box component="section" sx={{
@@ -37,16 +39,17 @@ function App() {
                     width: '97%',
                     alignItems: 'center'
                 }}>
-                    <Box component="section" sx={{
-                        p: 2,
-                        border: 1,
-                        borderColor: 'grey.500',
-                        borderRadius: '16px',
-                        boxShadow: 3,
-                        width: '97%',
-                        height: '100%'
-                    }}>
-                    </Box>
+                    <Stage width={128} height={128} className="border border-gray-500 shadow-xl">
+                        <Layer>
+                            <Image
+                                image={iconOVerlay}
+                                width={128}
+                                height={128}
+                                x={0}
+                                y={0}
+                            />
+                        </Layer>
+                    </Stage>
                     <ButtonGroup variant="contained" aria-label="Basic button group">
                         <Button>Upload Image</Button>
                         <Button>Download Wii U Icon</Button>
@@ -57,16 +60,7 @@ function App() {
                     width: '97%',
                     alignItems: 'center'
                 }}>
-                    <Box component="section" sx={{
-                        p: 2,
-                        border: 1,
-                        borderColor: 'grey.500',
-                        borderRadius: '16px',
-                        boxShadow: 3,
-                        width: '97%',
-                        height: '100%'
-                    }}>
-                    </Box>
+                    <div className="w-[500px] h-[500px] border border-gray-500 rounded-3xl shadow-xl" id="bannerCanvas"></div>
                     <ButtonGroup variant="contained" aria-label="Basic button group">
                         <Button>Upload Image</Button>
                         <Button>Download Wii U Banner</Button>
